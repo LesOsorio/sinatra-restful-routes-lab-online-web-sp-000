48fd01fb82fdf45ef266e60a9a4b07e134cebab2
Leslie Osorio <lesosorio17@gmail.com>
2019-12-12 01:56:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < Sinatra::Base
     redirect '/recipes'
   end
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    erb :show
+  end
+
+
 end
