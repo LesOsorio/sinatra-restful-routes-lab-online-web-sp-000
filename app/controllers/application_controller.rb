7d04ccc87e704eb961bab216d374a88c1d26c235
Leslie Osorio <lesosorio17@gmail.com>
2019-12-12 06:09:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/new' do
+    #binding.pry
     erb :new
   end
 
+  post '/recipes' do
+    Recipe.new(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      cook_time: params[:cook_time]
+      ).tap do |recipe|
+        recipe.save
+        redirect "/recipes/#{recipe.id}"
+    end
+  end
 
   get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
     erb :show
   end
 
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    #binding.pry
+    Recipe.find(params[:id]).tap do |recipe|
+      recipe.update(
+        name: params[:id],
+        ingredients: params[:ingredients],
+        cook_time: params[:cook_time]
+        )
+      redirect "/recipes/#{recipe.id}"
+    end
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.find(params[:id]).destroy
+    redirect '/recipes'
+  end
 
 end
